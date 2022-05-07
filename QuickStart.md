6ed8ea49be381011acde88b844f962c44aade481
Joseph C Wang <joequant@gmail.com>
2022-05-07 17:49:59 +0800

    truflation new-api

--- QuickStart.md
+++ QuickStart.md
@@ -4,16 +4,17 @@ Query Truflation's inflation data by following one of the guides below, based on
 
 ## 1. Testing with existing smart contract
 
-An existing smart contract is available for testing purposes on [Rinkeby testnet](https://rinkeby.etherscan.io/address/0x769dadc1bdBAF9d129D54981636Dcd96EdB57c0d). This contract fetches inflation data from Truflation's Rinkeby Chainlink node and stores the last result that it fetched.
+An existing smart contract is available for testing purposes on [Rinkeby testnet](https://rinkeby.etherscan.io/address/0x79053120810FdDf61ceFB781fc898D1cf52A44a0). This contract fetches inflation data from Truflation's Rinkeby Chainlink node and stores the last result that it fetched.
 
 This contract is made available only for demonstration purposes and should not be used in production by your application.
 
 To query the contract
-1. [Visit the "Read Contract" page on Rinkeby Etherscan](https://rinkeby.etherscan.io/address/0x769dadc1bdBAF9d129D54981636Dcd96EdB57c0d#readContract)
-2. Query the inflation value to see the last value fetched. To fetch a new value, navigate to ["Write Contract"](https://rinkeby.etherscan.io/address/0x769dadc1bdBAF9d129D54981636Dcd96EdB57c0d#writeContract) and call `RequestInflationData`. The call will not be made successfully if the contract is out of Rinkeby LINK. You can send the link fee of 0.01 LINK directly to the smart contract. Rinkeby testnet LINK
+1. [Visit the "Read Contract" page on Rinkeby Etherscan](https://rinkeby.etherscan.io/address/0x79053120810FdDf61ceFB781fc898D1cf52A44a0#readContract)
+2. Query the inflation value to see the last value fetched. click inflation under ["Read Contract"](https://rinkeby.etherscan.io/address/0x79053120810FdDf61ceFB781fc898D1cf52A44a0#readContract).  The inflation value is available as a string or as an integer.
+3. To fetch a new value, navigate to ["Write Contract"](https://rinkeby.etherscan.io/address/0x79053120810FdDf61ceFB781fc898D1cf52A44a0) and call `RequestInflationDataString` or `RequestInflationDataWei`. The call will not be made successfully if the contract is out of Rinkeby LINK. You can send the link fee of 0.01 LINK directly to the smart contract. Rinkeby testnet LINK
    can be acquired from [ChainLink faucet](https://faucets.chain.link/)
-3. wait for the node to respond
-4. click inflation under ["Read Contract"](https://rinkeby.etherscan.io/address/0x769dadc1bdBAF9d129D54981636Dcd96EdB57c0d#readContract)
+4. wait for the node to respond
+
 
 ## 2. Deploying your own smart contract
 
@@ -23,12 +24,12 @@ To query the contract
 4. navigate to "Solidity Compiler" from the side menu and click on "Compile TruflationTester.sol"
 5. navigate to "deploy & run transactions" from the side menu, set the "environment" to "Injected Web3", and log into your Metamask if necessary.
 6. set the "contract" field to `TruflationTester`
-7. set `address _link` to the appropriate [LINK token address](https://docs.chain.link/docs/link-token-contracts/) for your network (the Rinkeby address is `0x01BE23585060835E02B77ef475b0Cc51aA1e0709`)
+7. Insert the oracle id, job id, and fee (wei) in the contract
 8. press "deploy" and sign with Metamask, wait for the transaction to be confirmed
 9. when confirmed, your deployed contract will be visible in "deployed contracts" below the "deploy" button that you just pressed
 10. press the "copy" icon to the right of your contract name (shown as something like "TRUFLATIONTESTER AT 0x000..0000 (BLOCKCHAIN)") to get your contract's address
 11. send some Rinkeby (or relevant network) LINK directly to your smart contract's address via Metamask or other wallet (0.01 LINK per call)
-12. press the orange `requestInflationData` button to fetch inflation data from Chainlink and store the result in your contract
+12. press the orange `requestInflationDataString` or `requestInflationDataWei` button to fetch inflation data from Chainlink and store the result in your contract
 13. when confirmed, press the blue `inflation` button to see the result
 
 ## Interfacing with web
--- TruflationTester.sol
+++ TruflationTester.sol
@@ -4,66 +4,105 @@ pragma solidity ^0.8.13;
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
+
 contract TruflationTester is ChainlinkClient, ConfirmedOwner {
-    using Chainlink for Chainlink.Request;
+  using Chainlink for Chainlink.Request;
+  
+  string public inflationString;
+  uint256 public inflationWei;
+  using Chainlink for Chainlink.Request;
+  address public oracleId;
+  string public jobId;
+  uint256 public fee;
 
-    uint256 public inflation;
-    using Chainlink for Chainlink.Request;
-    address public oracle;
-    bytes32 public jobId;
-    uint256 public fee;
+  // Please refer to README and find corresponding chain's information
+  
+  // Ethereum mainnet
+  // oracle = 0xfE2dD37BC29f5fc4E0cad8F58F4Dbf4AddD5A59A;
+  // jobId = "b04c2a85143c43089c1befe7c41dea93";
+  
+  // BNB Smart Chain Mainnet
+  // oracle = 0x02a1BE5682f4Fcc941746e95f095c356A7f4D480;
+  // jobId = "b04c2a85143c43089c1befe7c41dea93";
+  
+  // Ethereum Rinkeby Testnet (Chain ID: 4)
+  // oracle = 0x17dED59fCd940F0a40462D52AAcD11493C6D8073;
+  // jobId = "b04c2a85143c43089c1befe7c41dea93";
+  // fee = 10**16
 
-    constructor(address _link) ConfirmedOwner(msg.sender) {
-        setChainlinkToken(_link);
+  constructor(
+    address oracleId_,
+    string memory jobId_,
+    uint256 fee_
+  ) ConfirmedOwner(msg.sender) {
+    setPublicChainlinkToken();
+    oracleId = oracleId_;
+    jobId = jobId_;
+    fee = fee_;
+  }
+
         
-        // Please refer to README and find corresponding chain's information
+  function requestInflationString() public returns (bytes32 requestId) {
+    Chainlink.Request memory req = buildChainlinkRequest(
+      bytes32(bytes(jobId)),
+      address(this),
+      this.fulfillInflationString.selector
+    );
+    req.add("service", "truflation/current");
+    req.add("keypath", "yearOverYearInflation");
+    req.add("abi", "json");
+    return sendChainlinkRequestTo(oracleId, req, fee);
+  }
 
-        // Ethereum mainnet
-        // oracle = 0xfE2dD37BC29f5fc4E0cad8F58F4Dbf4AddD5A59A;
-        // jobId = "b04c2a85143c43089c1befe7c41dea93";
+  function fulfillInflationString(
+    bytes32 _requestId,
+    bytes memory _inflation
+  ) public recordChainlinkFulfillment(_requestId) {
+    inflationString = string(_inflation);
+  }
 
-        // BNB Smart Chain Mainnet
-        // oracle = 0x02a1BE5682f4Fcc941746e95f095c356A7f4D480;
-        // jobId = "b04c2a85143c43089c1befe7c41dea93";
+  function requestInflationWei() public returns (bytes32 requestId) {
+    Chainlink.Request memory req = buildChainlinkRequest(
+      bytes32(bytes(jobId)),
+      address(this),
+      this.fulfillInflationWei.selector
+    );
+    req.add("service", "truflation/current");
+    req.add("keypath", "yearOverYearInflation");
+    req.add("abi", "uint256");
+    req.add("multiplier", "1000000000000000000");
+    return sendChainlinkRequestTo(oracleId, req, fee);
+  }
 
-        // Ethereum Rinkeby Testnet (Chain ID: 4)
-        oracle = 0x17dED59fCd940F0a40462D52AAcD11493C6D8073;
-        jobId = "b04c2a85143c43089c1befe7c41dea93";
+  function fulfillInflationWei(
+    bytes32 _requestId,
+    bytes memory _inflation
+  ) public recordChainlinkFulfillment(_requestId) {
+    inflationWei = toUint256(_inflation);
+  }
 
-        fee = 1 * 10 ** 16;
-    }
+  function changeOracle(address _oracle) public onlyOwner {
+    oracleId = _oracle;
+  }
+  
+  function changeJobId(string memory _jobId) public onlyOwner {
+    jobId = _jobId;
+  }
 
+  function getChainlinkToken() public view returns (address) {
+    return chainlinkTokenAddress();
+  }
 
-    function requestInflationData() public returns (bytes32 requestId) {
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-        return sendChainlinkRequestTo(oracle, request, fee);
-    }
-
-    function fulfill(bytes32 _requestId, uint256 _inflation) public recordChainlinkFulfillment(_requestId) {
-        inflation = _inflation;
-    }
-
-    function resetInflation() public {
-        inflation = 0;
-    }
-
-    function changeOracle(address _oracle) public onlyOwner {
-        oracle = _oracle;
-    }
-
-    function changeJobId(string memory _jobId) public onlyOwner {
-        jobId = bytes32(bytes(_jobId));
-    }
-
-    function getChainlinkToken() public view returns (address) {
-        return chainlinkTokenAddress();
-    }
-
-    function withdrawLink() public onlyOwner {
+  function withdrawLink() public onlyOwner {
     LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
     require(link.transfer(msg.sender, link.balanceOf(address(this))),
     "Unable to transfer");
   }
 
-
+  function toUint256(bytes memory _bytes) internal pure
+  returns (uint256 value) {
+    assembly {
+      value := mload(add(_bytes, 0x20))
+    }
+  }
 }
