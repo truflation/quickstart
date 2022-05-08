748c70467612035a4c6baeee20970847913bf50f
Joseph C Wang <joequant@gmail.com>
2022-05-08 16:20:07 +0800

    some refactoring

--- docs/index.js
+++ docs/index.js
@@ -4,10 +4,10 @@ window.addEventListener('load', function () {
   const showAccount = document.querySelector('.showAccount')
   const balanceAddress = document.querySelector('#balance\\:address')
   ethereumButton.addEventListener('click', () => {
-    getAccount()
+    initAccount()
   })
 
-  async function getAccount () {
+  async function initAccount () {
     accounts = await ethereum.request({ method: 'eth_requestAccounts' })
     showAccount.innerHTML = accounts[0]
     const inflationAddress =
@@ -32,66 +32,9 @@ window.addEventListener('load', function () {
     web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/<APIKEY>'))
   }
 })
-async function getBalance () {
-  const address = document.getElementById('balance:address').value
-  try {
-    const wei = await web3.eth.getBalance(address)
-    const balance = web3.utils.fromWei(wei, 'ether')
-    document.getElementById('balance:output').innerHTML = balance + ' ETH'
-  } catch (err) {
-    document.getElementById('balance:output').innerHTML = err
-  }
-}
 
-async function getInflation () {
-  try {
-    const address = document.getElementById('inflation:address').value
-    const contract = new web3.eth.Contract(inflation_abi, address)
-    document.getElementById('inflation:output').innerHTML =
-      await contract.methods.inflationString().call()
-  } catch (err) {
-    document.getElementById('inflation:output').innerHTML = err
-  }
-}
-
-async function refreshInflation() {
-  try {
-    const address = document.getElementById('inflation:address').value
-    const contract = new web3.eth.Contract(inflation_abi, address)
-    console.log(contract)
-    const link_token = await contract.methods.getChainlinkToken().call()
-    console.log(link_token)
-    document.getElementById('inflation:output').innerHTML = ''
-    const token_contract = new web3.eth.Contract(
-      erc20_abi, link_token
-    )
-    const fee = await contract.methods.fee().call()
-    console.log(fee)
-    const transfer = token_contract.methods.transfer(
-      address, fee
-    )
-    await transfer.send({
-      from: accounts[0],
-      to: address
-    })
-    const request = contract.methods.requestInflationString()
-    
-    const txn = await request.send({
-      from: accounts[0],
-      to: address
-    })
-    const id = txn.events.ChainlinkRequested.returnValues.id
-    contract.events.ChainlinkFulfilled({
-        filter: { id }
-      },
-      (error, event) => { console.log('value', error, event) })
-      .on('data', async (event) => {
-        document.getElementById('inflation:output').innerHTML =
-          await contract.methods.inflationString().call()
-      })
-  } catch (err) {
-    document.getElementById('inflation:output').innerHTML = err
-  }
+function getAccount() {
+  return accounts[0]
 }
 
 function hexStringToByteArray (hexString) {
